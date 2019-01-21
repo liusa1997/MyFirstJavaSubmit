d2b8969e685231fcef0007cbdd05b6800548ce22
liusa1997 <1984713349@qq.com>
2019-01-21 12:47:39 +0800

    THIS IS A JAVA_HELLOWORLD PROJECT

---
+++ src/helloworld/Hello_World.java
@@ -0,0 +1,18 @@
+package helloworld;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+shift+c快速多行注释
+		// ctrl+shift+f 快速格式化，注意这会被搜狗输入法占用
+		// 通过在references里面设置格式化属性，当文件保存时进行格式化
+//		System.out.println("hello world!!");
+//		System.out.println("hello world!!");
+//		System.out.println("hello world!!");
+		System.out.println("hello world!!");
+		System.out.println("hello world!!");
+		System.out.println("hello world!!");
+	}
+
+}
